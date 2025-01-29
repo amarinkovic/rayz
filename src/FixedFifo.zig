6832e58463301a9357084b91b8b93df7f0b1c6a3
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-29 23:29:17 +0100

    feat: genereic fifo [WiP]

--- src/FixedFifo.zig
+++ src/FixedFifo.zig
@@ -1,32 +1,48 @@
 const std = @import("std");
 
-pub const FixedFifo = struct {
-    items: []u32,
-    start: usize = 0,
-    end: usize = 0,
-    size: usize = 0,
+pub fn FixedFifo(comptime T: type) type {
+    return struct {
+        items: []T,
+        start: usize = 0,
+        end: usize = 0,
+        size: usize = 0,
 
-    pub fn init(allocator: *const std.mem.Allocator, capacity: usize) !FixedFifo {
-        return FixedFifo{
-            .items = try allocator.alloc(u32, capacity),
-        };
-    }
+        const Self = @This();
 
-    pub fn deinit(self: *FixedFifo, allocator: *const std.mem.Allocator) void {
-        allocator.free(self.items);
-    }
-
-    pub fn push(self: *FixedFifo, value: u32) void {
-        if (self.size == self.items.len) {
-            self.start = (self.start + 1) % self.items.len;
-        } else {
-            self.size += 1;
+        pub fn init(allocator: *const std.mem.Allocator, capacity: usize) !Self {
+            return Self{
+                .items = try allocator.alloc(T, capacity),
+            };
         }
-        self.items[self.end] = value;
-        self.end = (self.end + 1) % self.items.len;
-    }
 
-    pub fn get(self: *const FixedFifo, index: usize) u32 {
-        return self.items[(self.start + index) % self.items.len];
-    }
-};
+        pub fn deinit(self: *Self, allocator: *const std.mem.Allocator) void {
+            allocator.free(self.items);
+        }
+
+        pub fn push(self: *Self, value: T) void {
+            if (self.size == self.items.len) {
+                self.start = (self.start + 1) % self.items.len;
+            } else {
+                self.size += 1;
+            }
+            self.items[self.end] = value;
+            self.end = (self.end + 1) % self.items.len;
+        }
+
+        pub fn head(self: *Self) ?T {
+            if (self.size == 0) {
+                return null;
+            }
+
+            return self.items[self.end % self.items.len];
+        }
+
+        pub fn is_empty(self: *Self) bool {
+            return self.size == 0;
+        }
+
+        pub fn get(self: *const Self, index: usize) T {
+            return self.items[(self.start + index) % self.items.len];
+        }
+    };
+}
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 const rl = @import("raylib");
 // const rlm = @import("raylib-math"); // FIX: fix math module
-
-const FixedFifo = @import("./FixedFifo.zig").FixedFifo;
 const Ball = @import("./Ball.zig").Ball;
 
 const SCREEN_WIDTH = 800;
@@ -11,17 +9,16 @@ const SCREEN_HEIGHT = 450;
 pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
+
     const allocator = &arena.allocator();
 
-    var fifo = try FixedFifo.init(allocator, 3);
-    defer fifo.deinit(allocator);
+    var ball = try Ball.init(40, 40, 15, 5, allocator, 5);
+    defer ball.deinit(allocator);
 
     rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "rayz");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-
-    var ball = Ball.init(20, 20, 15, 5);
 
     //--------------------------------------------------------------------------------------
     // Main game loop
