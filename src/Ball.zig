1bafa884ade76681ba2c541e187085234dd23c65
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-29 23:29:17 +0100

    feat: genereic fifo [WiP]

--- src/Ball.zig
+++ src/Ball.zig
@@ -1,46 +1,60 @@
+const std = @import("std");
 const rl = @import("raylib");
+
+const FixedFifo = @import("./FixedFifo.zig").FixedFifo;
 
 const SCREEN_WIDTH = 800;
 const SCREEN_HEIGHT = 450;
 
 pub const Ball = struct {
-    position: [3]rl.Vector2,
+    position: FixedFifo(rl.Vector2),
     size: f32,
     speed: rl.Vector2,
 
-    pub fn init(x: f32, y: f32, size: f32, speed: f32) Ball {
+    pub fn init(x: f32, y: f32, size: f32, speed: f32, allocator: *const std.mem.Allocator, trail_size: usize) !Ball {
+        var fifo = try FixedFifo(rl.Vector2).init(allocator, trail_size);
+        fifo.push(rl.Vector2.init(x, y));
+
+        if (fifo.head()) |head| {
+            std.debug.print("      INIT HEAD >>>>>>>>> x = {}, y = {}\n", .{ head.x, head.y });
+        }
         return Ball{
-            .position = [3]rl.Vector2{
-                rl.Vector2.init(x, y),
-                rl.Vector2.init(x, y),
-                rl.Vector2.init(x, y),
-            },
+            .position = fifo,
             .size = size,
             .speed = rl.Vector2.init(speed, speed),
         };
     }
 
-    pub fn update(self: *Ball) void {
-        self.position[2] = self.position[1];
-        self.position[1] = self.position[0];
+    pub fn deinit(self: *Ball, allocator: *const std.mem.Allocator) void {
+        self.position.deinit(allocator);
+    }
 
-        self.position[0].x += self.speed.x;
-        self.position[0].y += self.speed.y;
+    pub fn update(self: *Ball) void {
+        if (self.position.head() == null) {
+            std.debug.print(" -- NO HEAD!!!", .{});
+        }
+
+        const head = self.position.head() orelse return;
+
+        const new_head = rl.Vector2.init(head.x + self.speed.x, head.y + self.speed.y);
+        self.position.push(new_head);
 
         // check for y collisions
-        if (self.position[0].y <= self.size or self.position[0].y + self.size >= SCREEN_HEIGHT) {
+        if (head.y <= self.size or head.y + self.size >= SCREEN_HEIGHT) {
             self.speed.y *= -1;
+            std.debug.print(" -- flip Y", .{});
         }
 
         // check for x collisions
-        if (self.position[0].x <= self.size or self.position[0].x + self.size >= SCREEN_WIDTH) {
+        if (head.x <= self.size or head.x + self.size >= SCREEN_WIDTH) {
             self.speed.x *= -1;
+            std.debug.print(" -- flip X", .{});
         }
     }
 
     pub fn draw(self: *Ball) void {
-        rl.drawCircleV(self.position[0], self.size, rl.Color.red);
-        rl.drawCircleV(self.position[1], self.size, rl.Color.red.alpha(0.7));
-        rl.drawCircleV(self.position[2], self.size, rl.Color.red.alpha(0.3));
+        if (self.position.head()) |head| {
+            rl.drawCircleV(head, self.size, rl.Color.red);
+        }
     }
 };
