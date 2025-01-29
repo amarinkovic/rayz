56ab79fab8e0799ff9ee45353febd2dba2302175
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-29 20:13:02 +0100

    feat: add fixed fifo

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,27 @@
-
+const std = @import("std");
 const rl = @import("raylib");
-// const rlm = @import("raylib-math");
+// const rlm = @import("raylib-math"); // FIX: fix math module
 
-const ball = @import("./Ball.zig");
+const FixedFifo = @import("./FixedFifo.zig").FixedFifo;
+const Ball = @import("./Ball.zig").Ball;
 
 const SCREEN_WIDTH = 800;
 const SCREEN_HEIGHT = 450;
 
 pub fn main() anyerror!void {
-    rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib-zig");
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator();
+
+    var fifo = try FixedFifo.init(allocator, 3);
+    defer fifo.deinit(allocator);
+
+    rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "rayz");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
 
-    var ballInstance = ball.Ball.init(20, 20, 15, 5);
+    var ball = Ball.init(20, 20, 15, 5);
 
     //--------------------------------------------------------------------------------------
     // Main game loop
@@ -31,8 +39,8 @@ pub fn main() anyerror!void {
 
         rl.clearBackground(rl.Color.black);
 
-        ballInstance.update();
-        ballInstance.draw();
+        ball.update();
+        ball.draw();
 
         rl.drawText("All you codebase are belong to us!", 190, 200, 20, rl.Color.green);
         //----------------------------------------------------------------------------------
