4b093dc0ff817cb92267880549c03a1d04e284b7
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-30 22:21:28 +0100

    chore: cleanup build file

--- build.zig
+++ build.zig
@@ -11,30 +11,7 @@ pub fn build(b: *std.Build) !void {
     });
 
     const raylib = raylib_dep.module("raylib");
-    // const raylib_math = raylib_dep.module("raylib-math");
     const raylib_artifact = raylib_dep.artifact("raylib");
-
-    //web exports are completely separate
-    if (target.query.os_tag == .emscripten) {
-        const exe_lib = try rlz.emcc.compileForEmscripten(b, "rayz", "src/main.zig", target, optimize);
-
-        exe_lib.linkLibrary(raylib_artifact);
-        exe_lib.root_module.addImport("raylib", raylib);
-        // exe_lib.root_module.addImport("raylib-math", raylib_math);
-
-        // Note that raylib itself is not actually added to the exe_lib output file, so it also needs to be linked with emscripten.
-        const link_step = try rlz.emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
-        //this lets your program access files like "resources/my-image.png":
-        link_step.addArg("--embed-file");
-        link_step.addArg("resources/");
-
-        b.getInstallStep().dependOn(&link_step.step);
-        const run_step = try rlz.emcc.emscriptenRunStep(b);
-        run_step.step.dependOn(&link_step.step);
-        const run_option = b.step("run", "Run rayz");
-        run_option.dependOn(&run_step.step);
-        return;
-    }
 
     const exe = b.addExecutable(.{ .name = "rayz", .root_source_file = b.path("src/main.zig"), .optimize = optimize, .target = target });
 
