601ce5f55bbf0a29e060b4085d41592b49c6904e
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-02-13 18:09:54 +0100

    feat: apply bloom shader to floor

--- src/main.zig
+++ src/main.zig
@@ -5,11 +5,11 @@ const SCREEN_WIDTH = 1200;
 const SCREEN_HEIGHT = 800;
 
 pub fn main() anyerror!void {
+    // window
     rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "rayz");
     defer rl.closeWindow(); // Close window and OpenGL context
 
-    // rl.setTargetFPS(60); // PERF: don't fix the framerate!
-
+    // camera
     var camera: rl.Camera3D = rl.Camera3D{
         .position = rl.Vector3.init(0, 20.0, 20.0),
         .target = rl.Vector3.init(0, 0, 0),
@@ -19,19 +19,33 @@ pub fn main() anyerror!void {
     };
     rl.disableCursor();
 
-    const floorImg = rl.genImageChecked(150, 150, 4, 4, rl.Color.blue, rl.Color.dark_blue);
+    // floor
+    const floorImg = rl.genImageChecked(151, 150, 4, 4, rl.Color.blue, rl.Color.dark_blue);
     const floorTexture = try rl.loadTextureFromImage(floorImg);
     const floor = try rl.loadModelFromMesh(rl.genMeshPlane(150, 150, 1, 1));
     rl.setMaterialTexture(floor.materials, rl.MaterialMapIndex.albedo, floorTexture);
 
-    const zero = rl.Vector3.zero();
+    defer rl.unloadTexture(floorTexture);
+    defer rl.unloadModel(floor);
 
     // blender model
     const spiral = try rl.loadModel("model/spiral_cubes.glb");
     const modelImage = rl.genImageCellular(200, 300, 30);
-    // const modelImage = rl.genImagePerlinNoise(100, 100, 10, 20, 0.4);
     const modelTexture = try rl.loadTextureFromImage(modelImage);
     rl.setMaterialTexture(spiral.materials, rl.MaterialMapIndex.albedo, modelTexture);
+
+    defer rl.unloadTexture(modelTexture);
+    defer rl.unloadModel(spiral);
+
+    // shader
+    const shader = try rl.loadShader(null, "resources/shaders/bloom.glsl");
+    const shaderTexture = try rl.loadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
+    defer rl.unloadShader(shader);
+    defer rl.unloadRenderTexture(shaderTexture);
+
+    const zero2 = rl.Vector2.zero();
+    const zero3 = rl.Vector3.zero();
+    const renderArea = rl.Rectangle{ .height = SCREEN_HEIGHT * -1, .width = SCREEN_WIDTH, .x = 0, .y = 0 };
 
     //---------[ MAIN LOOP ]----------------------------------------------------------------
 
@@ -43,18 +57,31 @@ pub fn main() anyerror!void {
         //----------------------------------------------------------------------------------
 
         //-----[ DRAW ]---------------------------------------------------------------------
+        rl.beginTextureMode(shaderTexture);
+        rl.clearBackground(rl.Color.black);
+
+        rl.beginMode3D(camera);
+
+        rl.drawModel(floor, zero3, 1, rl.Color.dark_brown);
+
+        rl.endMode3D();
+        rl.endTextureMode();
+
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.black);
 
+        rl.beginShaderMode(shader);
+        rl.drawTextureRec(shaderTexture.texture, renderArea, zero2, rl.Color.white);
+        rl.endShaderMode();
+
+        // normal 3D
         rl.beginMode3D(camera);
-
-        rl.drawModel(floor, zero, 1, rl.Color.dark_brown);
-        rl.drawModel(spiral, zero, 1, rl.Color.green);
-
+        rl.drawModel(spiral, zero3, 1, rl.Color.green);
         rl.endMode3D();
 
+        // 2d overlay
         rl.drawText(rl.textFormat("Raylib: %d fps", .{rl.getFPS()}), 10, 20, 20, rl.Color.green);
 
         //----------------------------------------------------------------------------------
